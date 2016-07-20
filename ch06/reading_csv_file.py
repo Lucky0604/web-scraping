8f126aa4c476f9ae3f264141ef00929b90cecc8d
Lucky <175917645@qq.com>
2016-07-20 23:22:52 +0800

    basic implemention reading pdfs to a string

--- ch06/reading_csv_file.py
+++ ch06/reading_csv_file.py
@@ -8,5 +8,17 @@ data = urlopen('http://pythonscraping.com/files/MontyPythonAlbums.csv').read().d
 dataFile = StringIO(data)
 csvReader = csv.reader(dataFile)
 
+'''
 for row in csvReader:
     print(row)
+
+#['Name', 'Year']
+#["Monty Python's Flying Circus", '1970']
+#['Another Monty Python Record', '1971']
+'''
+dictReader = csv.DictReader(dataFile)
+for row in dictReader:
+    print(row)
+#{'Year': '1970', 'Name': "Monty Python's Flying Circus"}
+#{'Year': '1971', 'Name': 'Another Monty Python Record'}
+#{'Year': '1972', 'Name': "Monty Python's Previous Record"}
